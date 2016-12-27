339189f37e8cc3da673c3442db4912391bd65f55
Koji Saiki <saikou9901@gmail.com>
2016-12-27 23:03:08 +0900

    coloring evils + redraw on type (change text)

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 'use strict';
 import * as vscode from 'vscode';
 
+var decChars: DecChars;
+
 export function activate(context: vscode.ExtensionContext) {
 
     let cmdInspect = vscode.commands.registerCommand('extension.inspect', () => {
@@ -9,14 +11,29 @@ export function activate(context: vscode.ExtensionContext) {
     let cmdSearchEvil = vscode.commands.registerCommand('extension.searchevil', () => {
         inspect();
     });
+        
+    // Also trigger an update on changing the editor
+    vscode.window.onDidChangeActiveTextEditor(editor => {
+        inspect();
+    }, null, context.subscriptions);
+    
+    // And when modifying the document
+    vscode.workspace.onDidChangeTextDocument(event => {
+        inspect();
+    }, null, context.subscriptions);
 
     context.subscriptions.push(cmdInspect);
     context.subscriptions.push(cmdSearchEvil);
 
     function inspect() {
-        var src : String;
+        var editor = vscode.window.activeTextEditor;
+        
+        if (decChars != undefined) {
+            decChars.decorator.dispose();
+        }
 
-        src = vscode.window.activeTextEditor.document.getText();
+        var src : string;
+        src = editor.document.getText();
 
         var cnt = 0;
         var ofs = 0;
@@ -29,10 +46,40 @@ export function activate(context: vscode.ExtensionContext) {
             cnt++;
         }
 
-        vscode.window.showInformationMessage('inspect! wow! : ' + cnt);
+        var regex = new RegExp('　', 'gm');
+        var match;
+        decChars = {
+            'chars': [],
+            'decorator': vscode.window.createTextEditorDecorationType({
+                'borderWidth': '1px',
+                'borderRadius': '2px',
+                'borderStyle': 'solid',
+                'light': {
+                    'backgroundColor': 'rgba(58, 70, 101, 0.3)',
+                    'borderColor': 'rgba(58, 70, 101, 0.4)'
+                },
+                'dark': {
+                    'backgroundColor': 'rgba(117, 141, 203, 0.3)',
+                    'borderColor': 'rgba(117, 141, 203, 0.4)'
+                }
+            })
+        }
+        while(match = regex.exec(src)) {
+            var startPos = editor.document.positionAt(match.index);
+            var endPos = editor.document.positionAt(match.index + match[0].length);
+            var range = new vscode.Range(startPos, endPos);
+            decChars.chars.push(range);
+        }
+
+        editor.setDecorations(decChars.decorator, decChars.chars);
     }
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class DecChars {
+    chars: vscode.Range[];
+    decorator: vscode.TextEditorDecorationType;
 }
